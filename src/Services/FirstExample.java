9694e16d0125dde631be71a7dd46019c7280c919
Hp <Hp@192.168.1.25>
2020-02-12 22:23:23 +0300

    Second commit

--- src/Services/FirstExample.java
+++ src/Services/FirstExample.java
@@ -4,5 +4,10 @@ public class FirstExample {
 
 	public static void main(String[] args) {
 		System.out.println("Start git projects");
+		//deneme ...
+		int i =3;
+		if(i>3) {
+			System.out.println("3 ten büyük bir sayi");
+		}
 	}
 }
